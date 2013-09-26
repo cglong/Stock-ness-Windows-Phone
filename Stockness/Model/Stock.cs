d8ebfb757aca577b8988bcac462ccf9072e41412
Chris Long <christopherglong@mac.com>
2013-09-25 23:43:52 -0400

    Stock model

---
+++ Stockness/Model/Stock.cs
@@ -0,0 +1,10 @@
+﻿namespace Stockness.Model
+{
+    public class Stock
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Delta { get; set; }
+    }
+}
