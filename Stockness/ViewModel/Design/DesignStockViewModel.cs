fbdcafaaef67a8274043326b2e54a0fb5a19ba7a
tross064 <tross64@gatech.edu>
2013-10-09 17:52:54 -0400

    Created UI for StockPage

--- Stockness/ViewModel/Design/DesignStockViewModel.cs
+++ Stockness/ViewModel/Design/DesignStockViewModel.cs
@@ -7,5 +7,46 @@ namespace Stockness.ViewModel.Design
         public DesignStockViewModel()
         {
         }
+        public string ApplicationTitle
+        {
+            get
+            {
+                return "Stockness";
+            }
+        }
+        public string StockName
+        {
+            get{
+                return "Microsoft Corporation";
+            }
+        }
+        public string StockSymbol
+        {
+            get
+            {
+                return "MSFT";
+            }
+        }
+        public string CurrPrice
+        {
+            get
+            {
+                return "33.01";
+            }
+        }
+        public string PriceChange
+        {
+            get
+            {
+                return "+0.06";
+            }
+        }
+        public string PercentChange
+        {
+            get
+            {
+                return "0.18%";
+            }
+        }
     }
 }
--- Stockness/ViewModel/IStockViewModel.cs
+++ Stockness/ViewModel/IStockViewModel.cs
@@ -2,5 +2,11 @@
 {
     public interface IStockViewModel
     {
+        string StockName { get; }
+        string StockSymbol { get; }
+        string ApplicationTitle { get; }
+        string CurrPrice { get; }
+        string PriceChange { get; }
+        string PercentChange { get; }
     }
 }
