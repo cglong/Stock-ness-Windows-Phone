41b380e8223bd661721a8d2d585a1d66cd54f945
jhong70 <jhong70@gatech.edu>
2013-10-23 17:49:44 -0400

    Added DeltaPriceColorConverter used in mainpage

---
+++ Stockness/Core/PriceDeltaColorConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace Stockness.Core
+{
+    public class PriceDeltaColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double && value != null)
+            {
+                if ( (double)value >=0)
+                {
+                    return new SolidColorBrush(Color.FromArgb(200, 107, 214, 121));
+                }
+                return new SolidColorBrush(Color.FromArgb(200, 245, 103, 32));
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
