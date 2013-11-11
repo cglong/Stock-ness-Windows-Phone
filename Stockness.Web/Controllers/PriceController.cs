549fb70f25321d154a2733c9f0657bf015a0f8b5
Chris Long <christopherglong@mac.com>
2013-11-11 18:40:56 -0500

    Return current price of stock

---
+++ Stockness.Web/Controllers/PriceController.cs
@@ -0,0 +1,17 @@
+﻿using Stockness.Model;
+using System.Web.Http;
+
+namespace Stockness.Web.Controllers
+{
+    public class PriceController : ApiController
+    {
+        public Stock Get(string symbol)
+        {
+            return new Stock
+            {
+                Symbol = symbol,
+                CurrentPrice = 50,
+            };
+        }
+    }
+}
