4fae269052ae24d69284cc72b074b73dc5e15896
Chris Long <christopherglong@mac.com>
2013-11-12 16:07:31 -0500

    Superclass for networked ViewModels

---
+++ Stockness/ViewModel/Runtime/NetworkViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+
+namespace Stockness.ViewModel.Runtime
+{
+    public abstract class NetworkViewModel : ViewModelBase
+    {
+        protected T GetObject<T>(Uri uri)
+        {
+            return default(T);
+        }
+    }
+}
