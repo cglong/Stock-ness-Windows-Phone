b5f9eb9b443ea9118b86a24a7bbfee32fcf6959e
Chris Long <christopherglong@mac.com>
2013-11-12 16:50:06 -0500

    Stock info updates when load finishes

--- Stockness/ViewModel/Runtime/StockViewModel.cs
+++ Stockness/ViewModel/Runtime/StockViewModel.cs
@@ -7,33 +7,46 @@ namespace Stockness.ViewModel.Runtime
     {
         private static string UriFormat = "http://localhost:5000/api/price?symbol={0}";
 
-        private Stock _stock;
         public Stock Stock
         {
             set
             {
                 Uri uri = new Uri(String.Format(UriFormat, value.Symbol));
-                _stock = GetObject<Stock>(uri);
+                Stock stock = GetObject<Stock>(uri);
+
+                StockSymbol = stock.Symbol;
+                StockName = stock.CompanyName;
+                CurrPrice = stock.Price;
+                PriceChange = stock.PriceChange;
+                PercentChange = stock.Percentage;
             }
         }
 
+        private string _stockSymbol;
+        private const string StockSymbolPropertyName = "StockSymbol";
         public string StockSymbol
         {
             get
             {
-                return _stock.Symbol;
+                return _stockSymbol;
             }
-            set
+            private set
             {
-                
+                Set<string>(StockSymbolPropertyName, ref _stockSymbol, value);
             }
         }
 
+        private string _stockName;
+        private const string StockNamePropertyName = "StockName";
         public string StockName
         {
             get
             {
-                return _stock.CompanyName;
+                return _stockName;
+            }
+            private set
+            {
+                Set<string>(StockNamePropertyName, ref _stockName, value);
             }
         }
 
@@ -45,27 +58,45 @@ namespace Stockness.ViewModel.Runtime
             }
         }
 
+        private double _currPrice;
+        private const string CurrPricePropertyName = "CurrPrice";
         public double CurrPrice
         {
             get
             {
-                return _stock.Price;
+                return _currPrice;
+            }
+            private set
+            {
+                Set<double>(CurrPricePropertyName, ref _currPrice, value);
             }
         }
 
+        private double _priceChange;
+        private const string PriceChangePropertyName = "PriceChange";
         public double PriceChange
         {
             get
             {
-                return _stock.PriceChange;
+                return _priceChange;
+            }
+            private set
+            {
+                Set<double>(PriceChangePropertyName, ref _priceChange, value);
             }
         }
 
+        private double _percentChange;
+        private const string PercentChangePropertyName = "PercentChange";
         public double PercentChange
         {
             get
             {
-                return _stock.Percentage;
+                return _percentChange;
+            }
+            private set
+            {
+                Set<double>(PercentChangePropertyName, ref _percentChange, value);
             }
         }
     }
