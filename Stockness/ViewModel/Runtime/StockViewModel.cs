5ed130075839ea41e01661b896a5bdc4707aaf18
Chris Long <christopherglong@mac.com>
2013-11-12 16:26:05 -0500

    Load stock from network and display its data

--- Stockness/ViewModel/Runtime/StockViewModel.cs
+++ Stockness/ViewModel/Runtime/StockViewModel.cs
@@ -1,6 +1,64 @@
-﻿namespace Stockness.ViewModel.Runtime
+﻿using Stockness.Model;
+using System;
+
+namespace Stockness.ViewModel.Runtime
 {
-    class StockViewModel : NetworkViewModel
+    public class StockViewModel : NetworkViewModel, IStockViewModel
     {
+        private Stock _stock;
+        private static string UriFormat = "http://localhost:5000/api/price?symbol={0}";
+
+        public string StockSymbol
+        {
+            get
+            {
+                return _stock.Symbol;
+            }
+            set
+            {
+                Uri uri = new Uri(String.Format(UriFormat, value));
+                _stock = GetObject<Stock>(uri);
+            }
+        }
+
+        public string StockName
+        {
+            get
+            {
+                return _stock.CompanyName;
+            }
+        }
+
+        public string ApplicationTitle
+        {
+            get
+            {
+                return "Stockness";
+            }
+        }
+
+        public double CurrPrice
+        {
+            get
+            {
+                return _stock.Price;
+            }
+        }
+
+        public double PriceChange
+        {
+            get
+            {
+                return _stock.PriceChange;
+            }
+        }
+
+        public double PercentChange
+        {
+            get
+            {
+                return _stock.Percentage;
+            }
+        }
     }
 }
