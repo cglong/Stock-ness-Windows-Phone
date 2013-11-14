8dd1f2ff13fcdfe02901ed4f3145d6c1aed3b9ef
Chris Long <christopherglong@mac.com>
2013-11-13 19:53:57 -0500

    Add method for POST calls

--- Stockness/ViewModel/Runtime/NetworkViewModel.cs
+++ Stockness/ViewModel/Runtime/NetworkViewModel.cs
@@ -8,9 +8,21 @@ namespace Stockness.ViewModel.Runtime
     {
         private IRestClient _client = new RestClient("http://localhost:9442/api");
 
-        protected void GetAsync<T>(RestRequest request, T message, Action<IRestResponse<T>> callback) where T : new()
+        protected void GetAsync<T>(IRestRequest request, T message, Action<IRestResponse<T>> callback) where T : new()
         {
             request.AddObject(message);
+            SendAsync<T>(request, callback);
+        }
+
+        protected void PostAsync<T>(IRestRequest request, T message, Action<IRestResponse<T>> callback) where T : new()
+        {
+            request.Method = Method.POST;
+            request.AddBody(message);
+            SendAsync<T>(request, callback);
+        }
+
+        private void SendAsync<T>(IRestRequest request, Action<IRestResponse<T>> callback) where T : new()
+        {
             _client.ExecuteAsync<T>(request, callback);
         }
     }
