57d11b3f8c8f6ee29a35dc279ec9e3182097118a
Chris Long <christopherglong@mac.com>
2013-11-25 00:05:10 -0500

    Empty login controller for dummy server

---
+++ Stockness.Web/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace Stockness.Web.Controllers
+{
+    public class LoginController : ApiController
+    {
+        // POST api/login
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
